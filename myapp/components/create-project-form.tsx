6344d19864fed9c25b3fd886ddb7ceaa19a2c62a
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-08 17:49:35 +0330

    Update create-project-form.tsx

--- myapp/components/create-project-form.tsx
+++ myapp/components/create-project-form.tsx
@@ -23,7 +23,7 @@ export default function CreateProjectForm() {
                                                                               <button
                                                                                       type="submit"
                                                                                               disabled={pending}
-                                                                                                      className="rounded-lg bg-black px-5 py-3 text-white disabled:opacity-50"
+                                                                                                      className="rounded-lg bg-blue-400 px-5 py-3 text-white disabled:opacity-50"
                                                                                                             >
                                                                                                                     {pending ? "Creating..." : "Create"}
                                                                                                                           </button>
