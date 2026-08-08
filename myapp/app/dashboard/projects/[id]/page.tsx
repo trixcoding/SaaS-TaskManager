7f2da444f9dffeb37d43521ecdee9d8ba1def9dd
trixcoding <79534613+trixcoding@users.noreply.github.com>
2026-08-08 19:07:12 +0330

    Update page.tsx

--- myapp/app/dashboard/projects/[id]/page.tsx
+++ myapp/app/dashboard/projects/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound, redirect } from "next/navigation";
-import Link from "next/navigation"; 
+import Link from "next/link"; 
 import { getCurrentUser } from "../../../../lib/auth";
 import { prisma } from "../../../../lib/prisma";
 
